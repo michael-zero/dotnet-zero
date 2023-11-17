0938cc69867f9cb0351ec72f04ff8d06181bd7fb
michael-zero <mlmbufpi@gmail.com>
2023-11-17 10:49:50 -0300

    repositorio generico

--- blog/Program.cs
+++ blog/Program.cs
@@ -32,7 +32,7 @@ namespace ZeroDataAcesss {
     }
 
     public static void ReadUsers(SqlConnection connection){
-     var repository = new UserRepository(connection);
+     var repository = new Repository<User>(connection);
      var users = repository.Get();
 
      foreach(var user in users){
---
+++ blog/Repositories/Repository.cs
@@ -0,0 +1,18 @@
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.Repositories {
+  public class Repository<T> where T : class {
+    private readonly SqlConnection _connection;
+
+    public Repository(SqlConnection connection)
+    { 
+       _connection = connection;
+    }
+  
+
+    public IEnumerable<T> Get(){
+        return  _connection.GetAll<T>();
+    }
+  }
+}
